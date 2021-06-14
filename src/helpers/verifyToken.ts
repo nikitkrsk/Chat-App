6d4e66969d4730fc8b66d1beba0be9ca722e479b
Nikita Atroshenko <nikita@airheadventures.com>
2021-06-14 22:38:36 +0300

    wip: remove deprecated middleware

--- src/helpers/verifyToken.ts
+++ src/helpers/verifyToken.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import JWTR from "jwt-redis";
+import redis from "redis";
+
 export interface IGetUserAuthInfoRequest extends Request {
     role: string // or any other type
   }
@@ -11,10 +13,11 @@ export const verifyToken = (
   /* tslint:disable:no-string-literal */
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-
+  const redisClient = redis.createClient();
+  const jwtr = new JWTR(redisClient);
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.JWT_KEY as string, (err: any, user: any) => {
+  jwtr.verify(token, process.env.JWT_KEY as string, (err: any, user: any) => {
     // console.log(err);
     // console.log(user);
     if (err) return res.sendStatus(403);
--- src/socket.ts
+++ src/socket.ts
@@ -1,34 +1,70 @@
-import { authorize } from "@thream/socketio-jwt";
 import { getRepository, Repository } from "typeorm";
 import { ChatUser } from "./entity";
 import { Server } from "socket.io";
-import { EMITS } from "./interfaces/socketEmits"
+import JWTR from "jwt-redis";
+import redis from "redis";
+
+import { EMITS } from "./interfaces/socketEmits";
 export class SocketService {
-  io: Server;
+  io;
   constructor(server) {
     this.io = new Server(server);
-    this.io.use(
-      authorize({
-        secret: process.env.JWT_KEY,
-      })
-    );
+    this.io.use(async (socket, next) => {
+      if (socket.handshake.query && socket.handshake.query.token) {
+        let payload;
+        // JWT REDIS FOR TOKEN
+        const redisClient = redis.createClient();
+        const jwtr = new JWTR(redisClient);
+        try {
+          payload = await jwtr.verify(
+            socket.handshake.query.token,
+            process.env.JWT_KEY
+          );
+          socket.decoded = payload;
+          // tslint:disable-next-line
+          console.log({ decodedJWT: socket.decoded });
+          next();
+        } catch (err) {
+          socket.decoded = "unauthorized";
+          // tslint:disable-next-line
+          console.log({ decodedJWT: socket.decoded, err });
+          next();
+        }
+      } else {
+        socket.decoded = "unauthorized";
+        next();
+        // next(new Error("Authentication error"));
+        // return socket.disconnect(true);
+      }
+    });
     this.io.on("connection", async (socket) => {
       // jwt payload of the connected client
       // tslint:disable-next-line
-      console.log(socket.decodedToken);
-      const userRepository: Repository<ChatUser> = getRepository(ChatUser);
-      const user: ChatUser = await userRepository.findOne({
-        where: { uuid: socket.decodedToken.uuid },
-      });
-      if (user === undefined) {
+      //   console.log(socket.decoded);
+      if (socket.decoded === "unauthorized") {
+        socket.emit(EMITS.LOGIN, `LOGOUT`);
+      } else {
+        setTimeout(
+          () => socket.emit(EMITS.LOGIN, `LOgOUT`),
+          Math.abs(
+            new Date(socket.decoded.exp * 1000).getTime() - new Date().getTime()
+          )
+        );
+        const userRepository: Repository<ChatUser> = getRepository(ChatUser);
+        const user: ChatUser = await userRepository.findOne({
+          where: { uuid: socket.decoded.uuid },
+        });
+        if (user === undefined) {
+          // tslint:disable-next-line
+          console.log("user not found");
+        }
         // tslint:disable-next-line
-        console.log("user not found");
+        //   console.log(user);
+        socket.emit(EMITS.LOGIN, `Hi ${user.username}`);
+        socket.broadcast.emit(EMITS.LOGIN, user.username);
+        socket.join(user.username);
       }
-      // tslint:disable-next-line
-      console.log(user);
-      socket.emit(EMITS.LOGIN, `Hi ${user.username}`);
-      socket.broadcast.emit(EMITS.LOGIN, user.username);
-      socket.join(user.username);
+
       // Message
       // socket.on('message', (data) => {
       //     if (exUser.email) {
